5eefb0fe08151ac5fb9f1a230409fef2519b9d4b
Achraf Sekri <43936833+achrafsekri@users.noreply.github.com>
2023-03-26 13:05:44 +0100

    added poll crud and working on poll answer

---
+++ src/pages/api/poll/[pollId]/index.ts
@@ -0,0 +1,95 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "../../auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+import { prisma } from "../../../../server/db";
+import { formatResponse } from "../../../../shared/sharedFunctions";
+
+type PatchBody = {
+  pollTitle: string;
+  options: {
+    id: string;
+    title: string;
+  }[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const { pollId } = req.query;
+    const poll = await prisma.Poll.findUnique({
+      where: {
+        id: pollId as string,
+      },
+      include: {
+        options: true,
+      },
+    });
+
+    if (!poll) {
+      res.status(400).json(formatResponse(null, "Poll not found", "400"));
+    } else {
+      res
+        .status(200)
+        .json(formatResponse(poll, "Poll fetched successfully", "200"));
+    }
+  }
+
+  if (req.method === "PATCH") {
+    const { pollId } = req.query;
+    const { pollTitle, options } = req.body as PatchBody;
+    try {
+      pollTitle &&
+        (await prisma.Poll.update({
+          where: {
+            id: pollId as string,
+          },
+          data: {
+            title: pollTitle,
+          },
+        }));
+
+      options?.map(async (option) => {
+        const optionExists = await prisma.PollOption.findUnique({
+          where: {
+            id: option.id,
+          },
+        });
+
+        if (optionExists) {
+          await prisma.PollOption.update({
+            where: {
+              id: option.id,
+            },
+            data: {
+              title: option.title,
+            },
+          });
+        } else {
+          res.status(400).json(formatResponse(null, "Option not found", "400"));
+        }
+      });
+      res.status(200).json(formatResponse(null, "Poll updated", "200"));
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(formatResponse(null, "error updating poll", "400"));
+    }
+  }
+
+  if (req.method === "DELETE") {
+    const { pollId } = req.query;
+    try {
+      await prisma.Poll.delete({
+        where: {
+          id: pollId as string,
+        },
+      });
+      res
+        .status(200)
+        .json(formatResponse(null, "Poll deleted successfully", "200"));
+    } catch (err) {
+      res.status(400).json(formatResponse(null, "error deleting poll", "400"));
+    }
+  }
+}
