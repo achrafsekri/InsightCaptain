8f998aa5ce0dd06f64ff719865a745198bbfaa30
Achraf Sekri <43936833+achrafsekri@users.noreply.github.com>
2023-03-27 13:51:58 +0100

    fixed surveys

--- src/pages/api/survey/[surveyId]/index.ts
+++ src/pages/api/survey/[surveyId]/index.ts
@@ -1,95 +1,83 @@
-import { NextApiRequest,NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 // import { authOptions } from "../../auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 import { prisma } from "../../../../server/db";
 import { formatResponse } from "../../../../shared/sharedFunctions";
 
-
 export default async function handler(
-
-    req: NextApiRequest,
-    res: NextApiResponse
-    
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    
+  if (req.method === "GET") {
+    try {
+      const { surveyId } = req.query;
+      const getSurvey = await prisma.survey.findUnique({
+        where: {
+          id: String(surveyId),
+        },
+        include: {
+          SurveyField: true,
+        },
+      });
+      if (!getSurvey) {
+        return res
+          .status(404)
+          .json(formatResponse(null, "Survey not found", "NOT_FOUND"));
+      }
 
-    if(req.method === "GET"){
-        try {
-            const { surveyId } = req.query;
-            const getSurvey = await prisma.survey.findUnique({
-
-                where:{
-                    id: String(surveyId)
-                },
-                select:{
-                    id: true,
-                    title: true,
-                    description: true,
-                }
-            });
-        if(!getSurvey) {
-        return res.status(404).json(formatResponse(null,"Survey not found","NOT_FOUND"));}
-            
-            return res.status(200).json(formatResponse(getSurvey,"Success","OK"));
-        } catch {
-            res.status(500).json({message:"Something went wrong"})
-        }
-    }else{ 
-
-
-        if(req.method === "PATCH"){
-            try{
-
-                const { surveyId } = req.query;
-                const { title , description } = req.body;
-                const { organizationId } = req.query;
-                const { caseStudyId } = req.query;
-
-                const updateSurvey = await prisma.survey.update({
-
-                    where:{
-
-                        id: String(surveyId)
-                    },
-                    data:{
-
-                        title: String(title),
-                        description: String(description),
-                        organizationId: String(organizationId),
-                        caseStudyId: String(caseStudyId),
-                        updatedAt: new Date()
-
-                    },   
-                });
-                
-               
-                return res.status(200).json(formatResponse(updateSurvey,"Success","OK"))
-            }catch{
-                res.status(500).json({message:"Something went wrong : Could'nt update survey"})
-            }
-        }
-
-
-        if(req.method === "DELETE"){
-            try{
-
-                const { surveyId } = req.query;
-
-                const deleteSurvey = await prisma.survey.delete({
-
-                    where:{
-
-                        id: String(surveyId)
-
-                    }
-                });
-                
-                return res.status(200).json(formatResponse(deleteSurvey,"Success","OK"))
-            } catch {
-                res.status(500).json({message:"Something went wrong : Could'nt delete survey"})
-            }
-        }
-        
+      return res.status(200).json(formatResponse(getSurvey, "Success", "OK"));
+    } catch {
+      res.status(500).json({ message: "Something went wrong" });
     }
+  } else {
+    if (req.method === "PATCH") {
+      try {
+        const { surveyId } = req.query;
+        const { title, description } = req.body;
+        const { organizationId } = req.query;
+        const { caseStudyId } = req.query;
+
+        const updateSurvey = await prisma.survey.update({
+          where: {
+            id: String(surveyId),
+          },
+          data: {
+            title: String(title),
+            description: String(description),
+            organizationId: String(organizationId),
+            caseStudyId: String(caseStudyId),
+            updatedAt: new Date(),
+          },
+        });
+
+        return res
+          .status(200)
+          .json(formatResponse(updateSurvey, "Success", "OK"));
+      } catch {
+        res
+          .status(500)
+          .json({ message: "Something went wrong : Could'nt update survey" });
+      }
+    }
+
+    if (req.method === "DELETE") {
+      try {
+        const { surveyId } = req.query;
+
+        const deleteSurvey = await prisma.survey.delete({
+          where: {
+            id: String(surveyId),
+          },
+        });
+
+        return res
+          .status(200)
+          .json(formatResponse(deleteSurvey, "Success", "OK"));
+      } catch {
+        res
+          .status(500)
+          .json({ message: "Something went wrong : Could'nt delete survey" });
+      }
+    }
+  }
 }
-    
--- src/pages/api/survey/index.ts
+++ src/pages/api/survey/index.ts
@@ -1,54 +1,55 @@
-import { NextApiRequest,NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 // import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 import { prisma } from "../../../server/db";
 import { formatResponse } from "../../../shared/sharedFunctions";
+import { SurveyFeild } from "@prisma/client";
 
-
-
+type expectedBody = {
+  title: string;
+  caseStudyId: string;
+  description?: string;
+  surveyFields?: SurveyFeild[];
+  organizationId: string;
+};
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
+  if (req.method === "POST") {
+    try {
+      const { title, description, surveyFields, caseStudyId, organizationId } =
+        req.body as expectedBody;
+      console.log(req.body);
+      console.log(req.query);
+      const createSurvey = await prisma.survey.create({
+        data: {
+          title: String(title),
+          description: String(description),
+          SurveyField: {
+            create: surveyFields,
+          },
 
-    if(req.method === "POST")
-    {
-        try {
-
-            const { title, description } = req.body;
-            const { organizationId } = req.query;
-            const { caseStudyId } = req.query;
-
-            //const { surveyFields }: { surveyFields: SurveyFeild[] } = req.body;
-
-            const createSurvey = await prisma.survey.create({
-               
-                data: {
-
-                    title: String(title),
-                    description: String(description),
-
-                    organization: {
-                      connect: {
-                        id: String(organizationId),
-                      },
-                    },
-
-                      caseStudy: {
-                        connect: {
-                          id: String(caseStudyId),
-                        },  
-                      },
-                    
-                }
-            })
-            return res.status(200).json(formatResponse(createSurvey,"Success","OK"))
-            
-        } catch {
-            res.status(500).json({message:"Something went wrong : Could not create survey"})
-        }
+          caseStudy: {
+            connect: {
+              id: caseStudyId,
+            },
+          },
+          organization: {
+            connect: {
+              id: organizationId,
+            },
+          },
+        },
+      });
+      return res
+        .status(200)
+        .json(formatResponse(createSurvey, "Success", "OK"));
+    } catch {
+      res
+        .status(500)
+        .json({ message: "Something went wrong : Could not create survey" });
     }
-  
+  }
 }
-
